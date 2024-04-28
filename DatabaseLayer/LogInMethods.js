2330ef37801ce33406cc7eda27fe5be3c699a910
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2024-04-27 22:35:10 -0500

    Made the method rename the '_id' attribute for 'dbId'

--- DatabaseLayer/LogInMethods.js
+++ DatabaseLayer/LogInMethods.js
@@ -9,11 +9,17 @@ const getLoginMethods = (db) =>
     };
 
     const getBasicEmployerInfo = async (email) => {
-        return await db.collection('Employer').findOne({ "email" : { "$eq" : email } }, { projection: { dbId: 1, name: 1, password: 1 } });
+        basicInfo = await db.collection('Employer').findOne({ "email" : { "$eq" : email } }, { projection: { _id: 1, name: 1, password: 1 } });
+        basicInfo.dbId = basicInfo._id;
+        delete basicInfo._id; // When create the dbId, we can delete all this.
+        return basicInfo; // It is just temporal
     };
 
     const getBasicProviderInfo = async (email) => {
-        return await db.collection('Provider').findOne({ "email" : { "$eq" : email } }, { projection: { dbId: 1, name: 1, password: 1 } });
+        basicInfo = await db.collection('Provider').findOne({ "email" : { "$eq" : email } }, { projection: { dbId: 1, name: 1, password: 1 } })
+        basicInfo.dbId = basicInfo._id;
+        delete basicInfo._id;
+        return basicInfo;
     };
 
     return {
