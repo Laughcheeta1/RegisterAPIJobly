6cebdbe5cecc72e45a1769fe39dbb31663a3bb7a
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2024-05-02 15:23:11 -0500

    Added a ; and removed unnecesary things

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
+// const cors = require('cors');
 const registerController = require('./ControllerLayer/RegisterController');
 const exceptionHandler = require('./Middlewares/ExceptionHandler');
-
-const apiGatewayURI = "";
 
 const app = express();
 
 app.use(morgan('dev'));
-app.use(cors(
-    {
-        origin: apiGatewayURI,
-        credentials: true
-    }
-));
+// app.use(cors(
+//     {
+//         origin: apiGatewayURI,
+//         credentials: true
+//     }
+// ));
 
 app.use(express.json());
 app.use('/API', registerController);
